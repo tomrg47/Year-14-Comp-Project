0a440855f866695b502fad8dbba8106dd6c79853
pointlessgamer <tomrg47@gmail.com>
2022-03-29 10:03:18 +0100

    made a new file to work on the options' menu to see if a way to avoid using a class is available as class does not seem to work with any obvious fix

--- GUIZERO version_NEW/Main.py
+++ GUIZERO version_NEW/Main.py
@@ -1,6 +1,6 @@
 from guizero import *
 from tkinter import filedialog
-
+from options_menu import slider_value
 
 def Browse_cmd():
     files = filedialog.askopenfiles()
@@ -13,21 +13,23 @@ class options_menu():
         self.colour = fnt_colour
         self.bg = bg_colour
 
-    def font_size_cmd(self, slider_value):
-        self.size = slider_value
-        title_text.size = self.size
-
     def n_win(self):
+        def font_size_cmd():
+            self.size = slider_value
+            title_text.size = slider_value
+
         self.win = Window(app, title='Options Menu')
         self.title = Text(self.win, text='Options Menu', size=self.size, color=self.colour)
-        self.slider = Slider(self.win, start=10, end=80, command=font_size_cmd)
+        slider = Slider(self.win, start=10, end=80, command=font_size_cmd)
 
 
 app = App()  # creates the file window
 app.title = "Main Menu"  # Names the created window
 
-title_text = Text(app, text='Image Recognision')  # Adds a Title within the window
+title_text = Text(app, text='Image Recognition')  # Adds a Title within the window
 options = options_menu(10, 'black', 'grey', app)
 Browse_Button = PushButton(app, text='Open Files', command=Browse_cmd)  # creates a Push button on the app window.
 Option_Button = PushButton(app, text='Options', command=options.n_win)
+Exit_Button = PushButton(app, text='Exit', command=quit)  # quits the app on the button click event
+
 app.display()  # loops the window to keep the display showing
