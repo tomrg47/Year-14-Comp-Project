b4aef9078e2c0aca38960e7f074eff7ffe124847
pointlessgamer <tomrg47@gmail.com>
2022-04-14 19:37:20 +0100

    Attempted to make the text colour changing function.

--- GUIZERO version_NEW/Main.py
+++ GUIZERO version_NEW/Main.py
@@ -1,7 +1,9 @@
 from guizero import *
 from tkinter import filedialog
+
 app = App()  # creates the file window
 app.title = "Main Menu"  # Names the created window
+
 
 def Browse_cmd():
     files = filedialog.askopenfiles()
@@ -24,19 +26,26 @@ def Browse_cmd():
 #         slider = Slider(self.win, start=10, end=80, command=font_size_cmd)
 
 def options():
-    win= Window(app, title='Options Menu')
+    win = Window(app, title='Options Menu')
+
+    def change_txt_colour(x, y):
+        title_text.color= txt_waffle[x,y].color
+
+
     def change_txt_size(slider_value):
         title_text.size = slider_value
         return slider_value
 
-
     slider = Slider(win, start=10, end=80, command=change_txt_size)
-
+    txt_waffle = Waffle(win, height=3, width=3, align="left", command=change_txt_colour)
+    txt_waffle[0, 0].color = "red"
+    txt_waffle[1, 0].color = "green"
+    txt_waffle[2, 0].color = "blue"
 # app = App()  # creates the file window
 # app.title = "Main Menu"  # Names the created window
 
 title_text = Text(app, text='Image Recognition')  # Adds a Title within the window
-#options = options_menu(10, 'black', 'grey', app)
+# options = options_menu(10, 'black', 'grey', app)
 Browse_Button = PushButton(app, text='Open Files', command=Browse_cmd)  # creates a Push button on the app window.
 Option_Button = PushButton(app, text='Options', command=options)
 Exit_Button = PushButton(app, text='Exit', command=quit)  # quits the app on the button click event
