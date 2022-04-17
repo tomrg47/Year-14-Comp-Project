7bcbd7bd8b441e6612e95c90746beb71ac32c023
pointlessgamer <tomrg47@gmail.com>
2022-04-17 10:38:52 +0100

    Options menu
    Options menu completed

--- GUIZERO version_NEW/Main.py
+++ GUIZERO version_NEW/Main.py
@@ -6,36 +6,46 @@ app.title = "Main Menu"  # Names the created window
 
 
 def Browse_cmd():
+    def Back_cmd():
+        title_text.show()
+        Browse_Button.show()
+        app.title = "Main Menu"
+        Sort_ttext.hide()
+        Sort_button.hide()
+        Back_button.hide()
+
+    def Sort_cmd():
+        open(exec('Neural_Network.py'))
+
     files = filedialog.askopenfiles()
-    return files
+    if len(files) >= 1:
+        title_text.hide()
+        Browse_Button.hide()
+        Option_Button.hide()
+        Exit_Button.hide()
+        app.title = "Sort Menu"
+        Sort_ttext = Text(app, text='Sort Menu')
+        Sort_button = PushButton(app, text='Sort', command=Sort_cmd)
+        Option_Button.show()
+        Back_button = PushButton(app, text='Back', command=Back_cmd)
+        Exit_Button.show()
 
+    else:
+        pass
 
-# class options_menu():
-#     def __init__(self, fnt_size, fnt_colour, bg_colour, app):
-#         self.size = fnt_size
-#         self.colour = fnt_colour
-#         self.bg = bg_colour
-#
-#     def n_win(self):
-#         def font_size_cmd():
-#             self.size = slider_value
-#             title_text.size = slider_value
-#
-#         self.win = Window(app, title='Options Menu')
-#         self.title = Text(self.win, text='Options Menu', size=self.size, color=self.colour)
-#         slider = Slider(self.win, start=10, end=80, command=font_size_cmd)
 
 def options():
     win = Window(app, title='Options Menu')
 
-    def change_bg_colour(x,y):
-        colour_bg = bg_waffle[x,y].color
+    def change_bg_colour(x, y):
+        colour_bg = bg_waffle[x, y].color
         app.bg = colour_bg
         win.bg = colour_bg
+
     def change_txt_colour(x, y):
         colour = txt_waffle[x, y].color
-        title_text.text_color= colour
-        test_text.text_color= colour
+        title_text.text_color = colour
+        test_text.text_color = colour
 
     def change_txt_size(slider_value):
         title_text.size = slider_value
@@ -43,7 +53,7 @@ def options():
         return slider_value
 
     slider = Slider(win, start=10, end=80, command=change_txt_size)
-    colours=["red","green","blue","pink","orange","black","grey","white","yellow"]
+    colours = ["red", "green", "blue", "pink", "orange", "black", "grey", "white", "yellow"]
     txt_waffle = Waffle(win, height=3, width=3, align="left", command=change_txt_colour)
     m = 0
     n = 0
@@ -54,19 +64,19 @@ def options():
             m = 0
             n = n + 1
 
-    bg_waffle =Waffle(win, height=3, width=3, align="right", command=change_bg_colour)
+    bg_waffle = Waffle(win, height=3, width=3, align="right", command=change_bg_colour)
     x = 0
     y = 0
-    for i in range(0,9):
-        bg_waffle[x, y].color=colours[i]
-        x = x+1
+    for i in range(0, 9):
+        bg_waffle[x, y].color = colours[i]
+        x = x + 1
         if x == 3:
             x = 0
-            y = y+1
-    test_text =Text(win, text = 'hello')
+            y = y + 1
+    test_text = Text(win, text='hello')
 
-# app = App()  # creates the file window
-# app.title = "Main Menu"  # Names the created window
+    close_button = PushButton(win, text='Close', command=win.destroy)
+
 
 title_text = Text(app, text='Image Recognition')  # Adds a Title within the window
 # options = options_menu(10, 'black', 'grey', app)
