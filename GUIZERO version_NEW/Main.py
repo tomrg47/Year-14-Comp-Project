0a580612c8954d17baab4e3e1beb372b5946c19c
pointlessgamer <tomrg47@gmail.com>
2022-05-18 18:07:40 +0100

    Final changes before submission

--- GUIZERO version_NEW/Main.py
+++ GUIZERO version_NEW/Main.py
@@ -8,7 +8,7 @@ from PIL import *
 import cv2
 from skimage import *
 
-awidth = 300
+awidth = 400
 aheight = 300
 
 app = App(height=aheight, width=awidth)  # creates the file window
@@ -23,7 +23,7 @@ test = ['', '']
 def Save_screen(r_Save_list, Urban_list, Sorting_txt):
     Sorting_txt.hide()
 
-    def rural():
+    def rural():  # saving function for rural(landscape) images
         print("hi")
         save = filedialog.asksaveasfilename(defaultextension=".*",
                                             filetypes=(
@@ -54,31 +54,43 @@ def Save_screen(r_Save_list, Urban_list, Sorting_txt):
             # resize = skimage.img_as_ubyte(resize)
             skimage.io.imsave(fname=str(combined), arr=image)
 
-    def urban():
+    def urban():  # The saving function for the Urban save button
         print("hello")
         saves = filedialog.asksaveasfilename(defaultextension=".*",
                                              filetypes=(
-                                                    ("Jpeg file", "*.jpg"), ("Jfif File", "*.jfif"), ("All Files", "*.*")))
-        if saves:
-            exts = saves
-            filetype = str(exts).split('.')[::-1]
-            combined2 = str(filetype[1]) + '.' + str(filetype[0])
-            image2 = Urban_list[0]
-            skimage.io.imsave(fname=str(combined2), arr=image2)
+                                                 ("Jpeg file", "*.jpg"), ("Jfif File", "*.jfif"), ("All Files", "*.*")))
+        # Above is the code to open the save as filedialog so the user can save the images where and
+        # under what ever name they want without disrupting the original.
+
+        if saves:  # if the user has typed in a name and selected a file extension this code runs
+            exts = saves  # place holder variable to get the name of the file and the file extension
+            filetype = str(exts).split('.')[::-1]  # splits the file type off of the user input file name
+            combined2 = str(filetype[1]) + '.' + str(filetype[0])  # combines the file type back with the file name
+            image2 = Urban_list[0]  # moves the data for the image need to a variable
+            skimage.io.imsave(fname=str(combined2), arr=image2)  # creates the file using the file name and declared
+            # type and writes the data to it.
 
     app.title = "Save Screen"
     app.update()
     Save_text = Text(app, text="Save Menu")
     Sort_text = Text(app, text="Sorting Complete")
+
     Landscape_save_txt = Text(app, text="Save Landscape Photos:")
-    l_Save_button = PushButton(app, text="Save Photos", command=rural)
+    if len(r_Save_list) >= 1:  # checks if any photos have been sorted into the catagory
+        l_Save_button = PushButton(app, text="Save Photos", command=rural)  # if there has displays save button
+    else:
+        l_lable = Text(app, text=' There are no Landscape Photos to save! ')  # if there isn't displays this text.
 
     Urban_save_txt = Text(app, text='Save Urban Photos:')
-    u_Save_button = PushButton(app, text="Save Photos", command=urban)
+    if len(Urban_list) >= 1:
+
+        u_Save_button = PushButton(app, text="Save Photos", command=urban)
+    else:
+        U_lable = Text(app, text=' There are no Urban Photos to save! ')
 
 
-def Browse_cmd():
-    def Back_cmd():
+def Browse_cmd():  # browse button function.
+    def Back_cmd():  # back button function
         title_text.show()
         Browse_Button.show()
         app.title = "Main Menu"
@@ -86,7 +98,7 @@ def Browse_cmd():
         Sort_button.hide()
         Back_button.hide()
 
-    def Sort_cmd():
+    def Sort_cmd():  # sort button function
         temp_List = []
         x = 0
         class_names = ["Landscape", "Urban"]
@@ -95,18 +107,15 @@ def Browse_cmd():
             Sort_ttext.hide()
             Sort_button.hide()
             Back_button.hide()
-            Sorting_txt = Text(app, text='Sorting...')
+            Sorting_txt = Text(app, text='Sorting...')  # displays while the neural network sorts the images
 
-            img = tf.keras.utils.load_img(files[x], target_size=(256, 256))  # 256,256 works
+            img = tf.keras.utils.load_img(files[x], target_size=(256, 256))  # 256,256 works # opens the images needed
+            # in a keras compatible format
 
-            img_array = tf.keras.utils.img_to_array(img)
-            img_array = tf.expand_dims(img_array, 0)
-            print(img_array)
-            testt = cv2.imread(files[x])
+            img_array = tf.keras.utils.img_to_array(img)  # adds all the images to an array
+            img_array = tf.expand_dims(img_array, 0)  # expands the dimensions of the array to an compatible size for
+            # the neual network
 
-            cv2.waitKey(0)
-            win = 'test'
-            cv2.imshow(win, testt)
             predictions = model.predict(img_array)
             print(predictions)
             score = tf.nn.softmax(predictions[0])
@@ -114,15 +123,11 @@ def Browse_cmd():
                 class_names[np.argmax(score)],
                 100 * np.max(score)))
 
-            if class_names[np.argmax(score)] == "Urban":
-                current_img2 = skimage.io.imread(files[x])
+            if class_names[np.argmax(score)] == "Urban":  # takes the argmax value of score to decide if it urban
+                current_img2 = skimage.io.imread(files[x])  # opens the current image in a compatible type for saving
                 Urban_list.append(current_img2)
-                print(Urban_list)
-            # for item2 in Urban_list:
-            #    temps = item2
-            #   print(temps.split('/', 1))
 
-            if class_names[np.argmax(score)] == "Landscape":
+            if class_names[np.argmax(score)] == "Landscape":  # takes argmax value of score to decide if rural
                 current_img = skimage.io.imread(files[x])
 
                 # Rural_list.append(files[x])
@@ -132,7 +137,7 @@ def Browse_cmd():
 
                 #  ttemp = temp.split('/', 1)[0][::-1]
 
-                r_Save_list.append(current_img)
+                r_Save_list.append(current_img)  # appends image to a list to be saved
 
                 # print(Rural_list)
 
@@ -152,7 +157,7 @@ def Browse_cmd():
         Exit_Button.hide()
         app.title = "Sort Menu"
         Sort_ttext = Text(app, text='Sort Menu')
-        Sort_button = PushButton(app, text='Sort', command=Sort_cmd)
+        Sort_button = PushButton(app, text='Sort', command=Sort_cmd) #  runs the images though the NN to be sorted
         Option_Button.show()
         Back_button = PushButton(app, text='Back', command=Back_cmd)
         Exit_Button.show()
@@ -164,28 +169,29 @@ def Browse_cmd():
 def options():
     win = Window(app, title='Options Menu', layout="auto", width=500)
 
-    def change_bg_colour(x, y):
+    def change_bg_colour(x, y):  # function for changing background colour
         colour_bg = bg_waffle[x, y].color
         app.bg = colour_bg
         win.bg = colour_bg
 
-    def change_txt_colour(x, y):
+    def change_txt_colour(x, y):  # function for changing text colour
         colour = txt_waffle[x, y].color
-        title_text.text_color = colour
-        test_text.text_color = colour
+        app.text_color = colour
+        win.text_color = colour
 
-    def change_txt_size(slider_value):
+    def change_txt_size(slider_value):  # function for changing text size
         app.text_size = slider_value
         win.text_size = slider_value
 
     colours = ["red", "green", "blue", "pink", "orange", "black", "grey", "white", None]
+    heading = Text(win, text='Text Colour     Text Size     Background Colour')
     txt_waffle = Waffle(win, height=3, width=3, align="left", command=change_txt_colour, pad=20)
     slider = Slider(win, start=10, end=80, command=change_txt_size, align='left')
 
     m = 0
     n = 0
     for v in range(0, 9):
-        txt_waffle[m, n].color = colours[v]
+        txt_waffle[m, n].color = colours[v]  # fills the waffle with the colours
         m = m + 1
         if m == 3:
             m = 0
@@ -195,20 +201,20 @@ def options():
     x = 0
     y = 0
     for i in range(0, 9):
-        bg_waffle[x, y].color = colours[i]
+        bg_waffle[x, y].color = colours[i]  # fills the waffle with the colours
         x = x + 1
         if x == 3:
             x = 0
             y = y + 1
-    global s_txt
+
     test_text = Text(win, text='hello', align="top")
 
-    close_button = PushButton(win, text='Close', command=win.destroy, align='bottom')
+    close_button = PushButton(win, text='Close', command=win.destroy, align='bottom')   # when clicked the program returns to the main menu
 
 
 title_text = Text(app, text='Image Recognition')  # Adds a Title within the window
 Browse_Button = PushButton(app, text='Open Files', command=Browse_cmd)  # creates a Push button on the app window.
 Exit_Button = PushButton(app, text='Exit', command=quit, align='bottom')  # quits the app on the button click event
-Option_Button = PushButton(app, text='Options', command=options, align='bottom')
+Option_Button = PushButton(app, text='Options', command=options, align='bottom') # The button to open the options menu.
 
 app.display()  # loops the window to keep the display showing
